827bca0f69b457168735f125055a15378d3c9c4b
rname97 <risalardan@gmail.com>
2025-01-26 02:29:58 +0700

    update project

--- src/page/UserPage.jsx
+++ src/page/UserPage.jsx
@@ -1,14 +1,12 @@
 
 import HeaderComponents from '../components/HeaderComponents'
 import ContentComponents from '../components/ContentComponents'
-import FooterComponents from '../components/FooterComponents'
 
 const UserPage = () => {
   return (
     <div>
         <HeaderComponents />
         <ContentComponents />
-        <FooterComponents />
     </div>
   )
 }
